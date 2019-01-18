5ea66a42ba068289024a9a95f000eded83a14ec5
Dhaivat Pandit <me@dhaiv.at>
2019-01-18 13:20:12 -0800

    dataproviders(airflow): replace window function with inner joins
    replacing window function with inner joins removes dependency on MySQL
    8, as Google Cloud Composer runs exclusively on 5.7 this enables Cloud
    Composer users to use discreETLy
    
    add sample cloud-composer k8s manifest

--- dashboard/dataproviders/airflow.py
+++ dashboard/dataproviders/airflow.py
@@ -12,16 +12,29 @@ class AirflowDBDataProvider:
         self.client = client
 
     def get_dag_state(self, dag_id, execution_date):
-        result = self.client.query(
-            f"select state from dag_run where dag_id = '{dag_id}' and execution_date = '{execution_date}'")
+        SQL = f"""
+        SELECT state FROM dag_run
+        WHERE dag_id = '{dag_id}'
+        AND execution_date = '{execution_date}'
+        """
+        result = self.client.query(SQL)
         return result[0]['state']
 
     def get_history(self, days):
         SQL = f'''
-        SELECT dag_id, date, state FROM (
-            SELECT dag_id, execution_date as date, state, ROW_NUMBER() OVER (PARTITION BY dag_id ORDER BY execution_date DESC) AS age
+        SELECT
+            a.dag_id,
+            a.execution_date as date,
+            a.state
+        FROM dag_run a
+        INNER JOIN (
+            SELECT dag_id, MAX(execution_date) as date
             FROM dag_run
-        ) t WHERE age <= {days}
+            WHERE execution_date >= DATE_ADD(CURDATE(), INTERVAL -{days} DAY)
+            GROUP BY dag_id
+        ) b ON
+            a.dag_id = b.dag_id
+            AND a.execution_date = b.date
         '''
         data = self.client.query(SQL)
 
@@ -52,11 +65,21 @@ class AirflowDBDataProvider:
 
     def get_newest_task_instances(self):
         newest_task_instances_sql = '''
-        SELECT dr.dag_id, dr.execution_date, dag_state, task_id, ti.state AS task_state, duration, start_date, end_date FROM (
-            SELECT dag_id, execution_date, state AS dag_state, ROW_NUMBER() OVER (PARTITION BY dag_id ORDER BY execution_date DESC) AS age 
-            FROM dag_run) dr 
-        JOIN task_instance ti ON ti.dag_id = dr.dag_id AND ti.execution_date = dr.execution_date
-        WHERE dr.age = 1'''.replace("\n", "")
+        SELECT dr.dag_id,
+            dr.execution_date,
+            ti.task_id,
+            ti.state as dag_state,
+            ti.duration,
+            ti.start_date,
+            ti.end_date
+        FROM (
+        SELECT dag_id,
+            MAX(execution_date) as execution_date
+        FROM dag_run
+            GROUP BY dag_id) dr
+        INNER JOIN task_instance ti
+        ON dr.dag_id = ti.dag_id AND
+        dr.execution_date = ti.execution_date'''.replace("\n", "")
 
         data = self.client.query(newest_task_instances_sql)
         result = {}
@@ -72,7 +95,7 @@ class AirflowDBDataProvider:
 
     def get_dag_tasks(self, dag_id, execution_date):
         data = self.client.query(
-            f"""SELECT dag_id, execution_date, task_id, start_date, end_date, duration, state as task_state 
+            f"""SELECT dag_id, execution_date, task_id, start_date, end_date, duration, state as task_state
             FROM task_instance WHERE dag_id='{dag_id}' AND execution_date='{execution_date}'""".replace("\n", ""))
         return [TaskInstance(**row) for row in data]
 
@@ -80,7 +103,7 @@ class AirflowDBDataProvider:
 
         statuses = '","'.join(
             reduce(
-                lambda x, y: x + y, 
+                lambda x, y: x + y,
                 list(report_tables.values())
                 )
             )
@@ -99,10 +122,19 @@ class AirflowDBDataProvider:
         '''
 
         latest_dags_status_sql = '''
-        SELECT * FROM (
-            SELECT dag_id, state, ROW_NUMBER() OVER (PARTITION BY dag_id ORDER BY execution_date DESC) AS age 
-            FROM dag_run) dr 
-        WHERE age = 1'''.replace("\n", "")
+        SELECT
+           dr.dag_id,
+           dr.state
+        FROM dag_run dr
+        INNER JOIN (SELECT
+                        dag_id,
+                        MAX(execution_date) AS date
+                    FROM dag_run
+                    GROUP BY dag_id) mx
+            ON
+            dr.dag_id = mx.dag_id
+            AND dr.execution_date = mx.date
+        '''.replace("\n", "")
         return [{
             'name': clean_dag_id(dag['dag_id']),
             'state': dag['state']}
